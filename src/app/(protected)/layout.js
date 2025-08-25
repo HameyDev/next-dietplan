beb54b745e0970643c95ebd81e408dc76a1ed5f4
M HAMMAD ZULFIQAR <mhammad.zulfiqar.se@gmail.com>
2025-08-25 18:14:55 +0500

    Update layout.js

--- src/app/(protected)/layout.js
+++ src/app/(protected)/layout.js
@@ -9,9 +9,16 @@ export default async function ProtectedLayout({ children }) {
   if (!session) redirect("/login");
 
   return (
-    <div className="min-h-screen bg-gray-50">
+    <div className="min-h-screen flex flex-col bg-gray-50">
+      {/* Navbar at top */}
       <Navbar />
-      <main className="max-w-6xl mx-auto px-4 py-6">{children}</main>
+
+      {/* Main content takes all available space */}
+      <main className="flex-1 max-w-6xl mx-auto px-4 py-6">
+        {children}
+      </main>
+
+      {/* Footer pushed to bottom */}
       <Footer />
     </div>
   );
